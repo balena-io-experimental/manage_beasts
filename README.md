ed9c305b544dee43f9928cd3e14b40c749ceb37e
Ronald McCollam <mccollam@gmail.com>
2017-01-18 12:30:06 -0500

    extend to allow arbitrary combined application targets

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # manage_beasts
 Quick and dirty shell scripts for managing demo environments for resin.io demos
 
-`./combine.sh` will pull all the beasts into a single application (called microBeastGlobal).
+`./combine.sh` will pull all the beasts into a single application, with an optional -a parameter to set the application target.  Default will be "microBeastGlobal".
 
 `./split.sh` will split the beasts into their individual applications.
 
--- combine.sh
+++ combine.sh
@@ -2,7 +2,40 @@
 
 source beasts
 
+TARGET="$APP_COMBINED"
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -a|--app)
+      TARGET="$2"
+      if [ "$TARGET" == "" ]
+      then
+        echo "ERROR: $1 requires an application name."
+        echo "(Try \"$0 -h\" for help.)"
+        exit 1
+      fi
+      shift
+    ;;
+    -h|--help)
+      echo -e "Usage:\n\n$0 [OPTIONS]\n"
+      echo "Option      Long Option     Meaning"
+      echo "-a <app>    --app <app>     Put all devices inside of the application named <app>"
+      echo "-h          --help          Show this help"
+      exit 0
+    ;;
+    *)
+      echo "ERROR: Unknown argument \"$1\"."
+      echo "(Try \"$0 -h\" for help.)"
+      exit 1
+    ;;
+  esac
+  shift
+done
+
+echo Moving all devices into "$TARGET"...
+
 for i in ${DEVICES_LONDON[@]} ${DEVICES_SEATTLE[@]}
 do
-  resin device move $i --application "$APP_COMBINED"
+  resin device move $i --application "$TARGET"
 done
